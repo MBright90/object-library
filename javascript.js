69bd65e7ebdee4d9bc9b1034f9a0ec990ec62b66
mbright90 <michael.bright90@icloud.com>
2022-07-27 10:36:31 +0100

    Add skeleton functions
    Creating a basic outline using function names and comments to show the
    expected flow of data. Subject to Change.

--- javascript.js
+++ javascript.js
@@ -13,7 +13,21 @@ function addBookToLibrary(bookObject) {
     return myLibrary;
 }
 
-// Creating initial book objects //
+function showBookForm() {
+    // Change css visibility attribute to visible on click
+}
+
+function parseFormData() {
+    // Retrieve form data
+}
+
+function createNewBook() {
+    // Use book constructor to take data from parseDormData
+    // and return a new book object
+}
+
+
+// Creating initial example book objects //
 
 const daVinci = new Book('The Da Vinci Code',
                          'Dan Brown',
@@ -28,3 +42,8 @@ const greatGatsby = new Book('The Great gatsby',
                              'Nick Carroway tells of his neighbour, the mysterious millionaire Jay Gatsby, and his obsession to reunite with his former lover, Nick\'s cousin',
                              'https://kbimages1-a.akamaihd.net/2411acbb-9daa-43fb-a5a2-a9aec064e17e/1200/1200/False/the-great-gatsby-238.jpg'
 );
+
+addBookToLibrary(daVinci);
+addBookToLibrary(greatGatsby);
+
+console.log(myLibrary)
