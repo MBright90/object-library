1a3449d20b65c2d8b3670e768417f4fcde370640
mbright90 <michael.bright90@icloud.com>
2022-08-01 14:02:54 +0100

    refactor book functions
    Moving several book related functions to utilities to be managed by
    the API manager class.

--- utilities/utilities.js
+++ utilities/utilities.js
@@ -2,10 +2,96 @@ export default class APIManager {
     constructor(endpoint, key) {
         this.endpoint = endpoint,
         this.key = key
+        this.bookShelf = [];
+    };
+
+    addBookToLibrary(title, author, year, description, imageURL) {
+        this.bookShelf.push(new Book(title,
+                                     author,
+                                     year || null,
+                                     description,
+                                     imageURL                  
+        ));
+        return true;
+    };
+
+    createNewCard(newBook) {
+        const cardDeck = document.querySelector('.card-deck')
+    
+        const cardTemplate =  document.createElement('div');
+        cardTemplate.classList.add('card');
+        cardTemplate.dataset.bookId = newBook.bookID;
+    
+        const cardImage = document.createElement('div');
+        cardImage.classList.add('image');
+        cardImage.style.backgroundImage = `url('${newBook.imageURL}')`;
+    
+        const bookInfo = document.createElement('div');
+        bookInfo.classList.add('book-info');
+    
+        const bookTitle = document.createElement('h3');
+        const titleText = document.createTextNode(newBook.title);
+        bookTitle.appendChild(titleText);
+        
+        const bookAuthor = document.createElement('h3');
+        const authorText = document.createTextNode(newBook.author);
+        bookAuthor.appendChild(authorText);
+        
+        const bookDescription = document.createElement('p');
+        const descriptionText = document.createTextNode(newBook.description);
+        bookDescription.appendChild(descriptionText);
+    
+        const readButton = document.createElement('a');
+        readButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+        if (newBook.hasRead) {
+            readButton.style = 'background-color: #3CCF4E; color: #FFFFFF';
+        };
+    
+        const deleteButton = document.createElement('a');
+        deleteButton.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+    
+        bookInfo.appendChild(bookTitle);
+        bookInfo.appendChild(bookAuthor);
+        bookInfo.appendChild(bookDescription);
+        bookInfo.appendChild(readButton);
+        bookInfo.appendChild(deleteButton);
+    
+        cardTemplate.appendChild(cardImage);
+        cardTemplate.appendChild(bookInfo)
+    
+        cardDeck.appendChild(cardTemplate);
+    
+        readButton.addEventListener('click', (e) => {
+            let bookID = parseInt(e.composedPath()[3].dataset.bookId);
+            this.bookShelf.forEach(book => {
+                if (book.bookID === bookID) {
+                    if (!book.hasRead) {
+                        e.composedPath()[1].style= 'background-color: #3CCF4E; color: #FFFFFF';
+                        book.hasRead = true;
+                    } else {
+                        e.composedPath()[1].style = 'background-color: ; color: inherit;';
+                        book.hasRead = false;
+                    };
+                };
+            });
+        });
+    
+        deleteButton.addEventListener('click', (e) => {
+            if (window.confirm('Delete card?')) {
+                this.bookShelf.forEach(book => {
+                    if (book.bookID === e.composedPath()[3].dataset.bookId) {
+                        console.log(book)
+                        let bookIndex = this.bookShelf.indexOf(book); 
+                        this.bookShelf.splice(bookIndex, 1)
+                    }
+                })
+                e.composedPath()[3].remove();
+            };
+        });
     };
 
     returnTitle(title) {
-        let book = fetch(`${this.endpoint}${title}:inauthor:orderBy=relevance&key=${this.key}`)
+        let book = fetch(`${this.endpoint}${title}:inTitle:orderBy=relevance&key=${this.key}`)
             .then(response => response.json())
             .then((response) => {
                 let bookInfo = response.items[0].volumeInfo;
@@ -23,4 +109,19 @@ export default class APIManager {
         return book;
     };
 
+};
+
+let bookIDCreator = 0;
+
+class Book {
+    constructor(title, author, year, description, imageURL) {
+        this.title = title.trim(),
+        this.author = author,
+        this.year = year,
+        this.description = description,
+        this.imageURL = imageURL,
+        this.hasRead = false,
+        this.bookID = bookIDCreator + 1,
+        bookIDCreator += 1
+    };
 };
