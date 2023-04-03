846818b226f96603dbfd211d52ef3237f829a00b
mbright90 <michael.bright90@icloud.com>
2023-04-03 09:53:36 +0100

    Add mock firebase config file

---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "API_KEY",
+  authDomain: "AUTH_DOMAIN",
+  projectId: "PROJECT_ID",
+  storageBucket: "STORAGE_BUCKET",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
+}
+
+export default firebaseConfig
