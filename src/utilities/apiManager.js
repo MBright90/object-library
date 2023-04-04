2995bea5f51d67da72673ca1404d375899bf44e8
mbright90 <michael.bright90@icloud.com>
2023-04-04 12:05:24 +0100

    Clean up file

--- src/utilities/apiManager.js
+++ src/utilities/apiManager.js
@@ -158,40 +158,40 @@ export default class APIManager {
   }
 }
 
-// experimenting with object initiation in place of class.
-const sortingProto = {
-  compareBooksYear(a, b) {
-    if (
-      document.querySelector(".sort-year-button").dataset.sortingOrder ===
-      "ascending"
-    ) {
-      return a.year < b.year ? 1 : -1
-    }
-    return a.year < b.year ? -1 : 1
-  },
+// // experimenting with object initiation in place of class.
+// const sortingProto = {
+//   compareBooksYear(a, b) {
+//     if (
+//       document.querySelector(".sort-year-button").dataset.sortingOrder ===
+//       "ascending"
+//     ) {
+//       return a.year < b.year ? 1 : -1
+//     }
+//     return a.year < b.year ? -1 : 1
+//   },
 
-  compareBooksAZ(a, b) {
-    function checkTitle(title) {
-      const thePattern = /^(\bthe\b)/i // Removes 'the' from beginning of book titles, case insensitive
+//   compareBooksAZ(a, b) {
+//     function checkTitle(title) {
+//       const thePattern = /^(\bthe\b)/i // Removes 'the' from beginning of book titles, case insensitive
 
-      if (title.match(thePattern)) {
-        title = title.substring(title.indexOf(" ") + 1)
-      }
+//       if (title.match(thePattern)) {
+//         title = title.substring(title.indexOf(" ") + 1)
+//       }
 
-      return title
-    }
+//       return title
+//     }
 
-    const titleA = checkTitle(a.title.toLowerCase())
-    const titleB = checkTitle(b.title.toLowerCase())
+//     const titleA = checkTitle(a.title.toLowerCase())
+//     const titleB = checkTitle(b.title.toLowerCase())
 
-    if (
-      document.querySelector(".sort-az-button").dataset.sortingOrder ===
-      "ascending"
-    ) {
-      return titleA < titleB ? 1 : -1
-    }
-    return titleA < titleB ? -1 : 1
-  },
-}
+//     if (
+//       document.querySelector(".sort-az-button").dataset.sortingOrder ===
+//       "ascending"
+//     ) {
+//       return titleA < titleB ? 1 : -1
+//     }
+//     return titleA < titleB ? -1 : 1
+//   },
+// }
 
-export const sortingObject = () => Object.assign(Object.create(sortingProto))
+// export const sortingObject = () => Object.assign(Object.create(sortingProto))
