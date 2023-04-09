0f4e4f7a02e53d5692cd734ff4de44703d567d5a
mbright90 <michael.bright90@icloud.com>
2023-04-09 10:27:09 +0100

    Move stat funcs from main to utils

--- src/utilities/utils.js
+++ src/utilities/utils.js
@@ -1,3 +1,23 @@
-export default function appendChildren(parent, ...children) {
+export function appendChildren(parent, ...children) {
   children.forEach((child) => parent.append(child))
+}
+
+// Stats utilities //
+
+const totalBooksPara = document.querySelector(".total-books")
+const booksReadPara = document.querySelector(".books-read")
+const totalPagesPara = document.querySelector(".total-pages")
+const pagesReadPara = document.querySelector(".pages-read")
+
+export async function updateStats(currentStats) {
+  totalBooksPara.textContent = currentStats.bookCount
+  booksReadPara.textContent = currentStats.booksRead
+  totalPagesPara.textContent = currentStats.totalPages
+  pagesReadPara.textContent = currentStats.pagesRead
+}
+
+export function showStatsPage(statsPage) {
+  if (statsPage.style.visibility !== "visible") {
+    statsPage.style.visibility = "visible"
+  }
 }
